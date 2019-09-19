36783580d1b44fa8d92b3b3d142a94c216c9ea8a
d <d>
2019-09-19 15:34:00 -0300

    Fix error ID:12212

--- escuelita.ci.api/escuelita.ci.api/Controllers/LoginController.cs
+++ escuelita.ci.api/escuelita.ci.api/Controllers/LoginController.cs
@@ -9,7 +9,7 @@ namespace escuelita.ci.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class LoginController : ControllerBase
+    public class LoginController : ControllerBase{}
     {
         // POST api/login
         [HttpPost]
